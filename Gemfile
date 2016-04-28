0b0866cd35edf0c0e6c1fa6c90bd9fee8c4e3c47
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-04-28 00:43:13 -0300

    snippet para cargar los pedidos del sftp

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Para el servidor
 gem 'puma'
+# Para conectar sftp
+gem 'net-sftp'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/oc_controller.rb
@@ -0,0 +1,20 @@
+class OcController < ApplicationController
+  def sftp
+    require 'net/sftp'
+    Net::SFTP.start('mare.ing.puc.cl', 'integra2', :password => 'fUgW9wJG') do |sftp|
+      # download a file or directory from the remote host
+      #sftp.download!("/pedidos", "public/pedidos", :recursive => true)
+      # list the entries in a directory
+      sftp.dir.foreach("/pedidos") do |entry|
+        if entry.name[0]!="."
+          puts "/pedidos/"+entry.name
+          data = sftp.download!("/pedidos/"+entry.name)
+          id = three_letters = data[/<id>(.*?)<\/id>/m, 1]
+          sku = three_letters = data[/<sku>(.*?)<\/sku>/m, 1]
+          qty = three_letters = data[/<qty>(.*?)<\/qty>/m, 1]
+          puts "ID: "+id+" // sku: "+ sku + " // qty: "+qty
+        end
+      end
+    end
+  end
+end
