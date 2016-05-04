74868a02747db99994b4905b640e13e01b9213e8
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-04 07:00:48 -0300

    fix json format on pagosCtrlr

--- app/controllers/api/pagos_controller.rb
+++ app/controllers/api/pagos_controller.rb
@@ -10,7 +10,7 @@ class Api::PagosController < ApplicationController
     idfactura = params[:idfactura]
     if !idfactura || !idtrx
       puts "--------Parámetros Incorrectos-------"
-      render json: {"validado": false, idtrx: idtrx.to_s}, status: 400 and return
+      render json: {"error": "Parámetros Incorrectos", "validado": false, "idtrx": idtrx.to_s}, status: 400 and return
     end
     puts "----idtrx: " + idtrx + "----idfactura: " + idfactura
     trx = obtener_transaccion(idtrx)
@@ -19,16 +19,16 @@ class Api::PagosController < ApplicationController
     puts "factura: " + factura.to_s
     if !trx['monto']
       puts "-----TRX OBTENIDA INVÁLIDA!-----"
-        render json: {"error": "TRX OBTENIDA INVÁLIDA!", "validado": false, idtrx: idtrx.to_s}, status: 400 and return
+        render json: {"error": "TRX OBTENIDA INVÁLIDA!", "validado": false, "idtrx": idtrx.to_s}, status: 400 and return
     elsif (Oc.find_by idfactura: idfactura) == nil
       puts "-----No tenemos la factura pagada en nuestro sistema-----"
-        render json: {"error": "No tenemos la factura en nuestro sistema", "validado": false, idtrx: idtrx.to_s}, status: 400 and return
+        render json: {"error": "No tenemos la factura en nuestro sistema", "validado": false, "idtrx": idtrx.to_s}, status: 400 and return
     elsif !factura['total']
       puts "-----FACTURA OBTENIDA INVÁLIDA!-----"
-        render json: {"error": "FACTURA OBTENIDA INVÁLIDA!", "validado": false, idtrx: idtrx.to_s}, status: 400 and return
+        render json: {"error": "FACTURA OBTENIDA INVÁLIDA!", "validado": false, "idtrx": idtrx.to_s}, status: 400 and return
     elsif factura['total'] != trx['monto']
       puts "------El pago no coincide con el monto de la factura-----"
-        render json: {"error": "El pago no coincide con el monto de la factura", "validado": false, idtrx: idtrx.to_s}, status: 400 and return
+        render json: {"error": "El pago no coincide con el monto de la factura", "validado": false, "idtrx": idtrx.to_s}, status: 400 and return
     end
     # Seguimos con el tema de la factura en un Thread aparte, para
     # no demorar la entrega de la respuesta
@@ -40,7 +40,7 @@ class Api::PagosController < ApplicationController
       despachado = despachar(idfactura, factura) #Función definida en application_controller
       avisar_a_grupo(factura['cliente'], idfactura)
     #end
-    render json: {"validado": true, idtrx: idtrx.to_s}
+    render json: {"validado": true, "idtrx": idtrx.to_s}
   end
 
   private
