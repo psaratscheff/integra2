0fe004bcd5f3dca27b9732ff1243dc43a9ca86e8
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-07 18:32:41 -0300

    bodega pass production

--- app/helpers/hmac_helper.rb
+++ app/helpers/hmac_helper.rb
@@ -3,8 +3,8 @@ module HmacHelper
   def encode(base)
     require 'base64'
     require 'hmac-sha1'
-    
-    key="O9$vSnqQ3Cm&5jZ"
+
+    key="79m7j#daV&q8eF"
   	result = Base64.encode64((HMAC::SHA1.new(key) << base).digest).strip
   	return result
   end
