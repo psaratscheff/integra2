895867bf3bd7c7e86a6be7ed8e1ac4a8be28ef31
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-12 18:38:31 -0300

    procesar ftp time fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 	# Cuando el metodo este listo hay que agregarle que procese solo las oc que no han sido procesadas ya
 	def procesar_sftp
 		puts "------------INICIANDO PROCESO SFTP EN BACKGROUND----------------"
-		background do # Función background definida en ApplicationController
+		#background do # Función background definida en ApplicationController
 			if $ambiente
 					urlServidor = 'moto.ing.puc.cl'
 					pass = '7ezT4Mz6'
@@ -10,95 +10,102 @@ class TasksController < ApplicationController
 					urlServidor = 'mare.ing.puc.cl'
 					pass = 'fUgW9wJG'
 			end
-			Net::SFTP.start(urlServidor, 'integra2', :password => pass) do |sftp|
-				count = 0
-				# download a file or directory from the remote host
-				#sftp.download!("/pedidos", "public/pedidos", :recursive => true)
-				# list the entries in a directory
-				sftp.dir.foreach("/pedidos") do |entry|
-					if entry.name[0]!="." && (((Time.now.to_i - 3600).to_s + "000").to_i - 3600000 < entry.name.split('.')[0].to_i)
-						data = sftp.download!("/pedidos/"+entry.name)
-						id = three_letters = data[/<id>(.*?)<\/id>/m, 1]
-						sku = three_letters = data[/<sku>(.*?)<\/sku>/m, 1]
-						qty = three_letters = data[/<qty>(.*?)<\/qty>/m, 1]
-						puts "ID: "+id+" // sku: "+ sku + " // qty: "+qty
+			begin
+				Net::SFTP.start(urlServidor, 'integra2', :password => pass) do |sftp|
+					count = 0
+					# download a file or directory from the remote host
+					#sftp.download!("/pedidos", "public/pedidos", :recursive => true)
+					# list the entries in a directory
+					sftp.dir.foreach("/pedidos") do |entry|
+						if entry.name[0]!="." && ((((Time.now.to_i).to_s + "000").to_i - 172800000)  < entry.name.split('.')[0].to_i) # Pedidos de los últimos dos días
+							puts (entry.name.split('.')[0].to_i).to_s
+							data = sftp.download!("/pedidos/"+entry.name)
+							id = three_letters = data[/<id>(.*?)<\/id>/m, 1]
+							sku = three_letters = data[/<sku>(.*?)<\/sku>/m, 1]
+							qty = three_letters = data[/<qty>(.*?)<\/qty>/m, 1]
+							puts "ID: "+id+" // sku: "+ sku + " // qty: "+qty
 
-						# Metodo definidos en application_controller
-						stock = consultar_stock(sku).to_i
-						qty = qty.to_i
-						# Revisamos que tengamos stock del producto y que sea un producto que nosotros producimos
-						if stock >= qty && (sku == "2" || sku == "12" || sku == "21" || sku == "28" || sku == "32")
-							# puts "------------------------>  Tengo stock del producto y soy proveedor de este"
-							# Obtener OC - Metodo definidos en application_controller
-							oc = obtener_oc(id)
+							# Metodo definidos en application_controller
+							stock = consultar_stock(sku).to_i
+							qty = qty.to_i
+							# Revisamos que tengamos stock del producto y que sea un producto que nosotros producimos
+							if stock >= qty && (sku == "2" || sku == "12" || sku == "21" || sku == "28" || sku == "32")
+								# puts "------------------------>  Tengo stock del producto y soy proveedor de este"
+								# Obtener OC - Metodo definidos en application_controller
+								oc = obtener_oc(id)
 
-							validado = false
-							# Ahora validamos que el precio que nos pagaran sea el mismo que tenemos o mayor a este
-							case sku
-							when "2"
-								validado = oc["precioUnitario"] >= 718
-							when "12"
-								validado = oc["precioUnitario"] >= 7413
-							when "21"
-								validado = oc["precioUnitario"] >= 1462
-							when "28"
-								validado = oc["precioUnitario"] >= 2382
-							when "32"
-								validado = oc["precioUnitario"] >= 1135
-							end
+								validado = false
+								# Ahora validamos que el precio que nos pagaran sea el mismo que tenemos o mayor a este
+								case sku
+								when "2"
+									validado = oc["precioUnitario"] >= 718
+								when "12"
+									validado = oc["precioUnitario"] >= 7413
+								when "21"
+									validado = oc["precioUnitario"] >= 1462
+								when "28"
+									validado = oc["precioUnitario"] >= 2382
+								when "32"
+									validado = oc["precioUnitario"] >= 1135
+								end
 
-							oc = transform_oc(oc)
-							localOc = Oc.new(oc)
-							localOc.save!
+								oc = transform_oc(oc)
+								localOc = Oc.new(oc)
+								localOc.save!
 
-							if validado
-								count = count + 1
-								puts "------------------------> Voy a aceptar la OC"
-								aceptar_oc(id)
-								# puts "------------------------> He aceptado oc"
-								# Generar facura para OC
-								# puts "------------------------> Voy a generar la factura"
-								factura = generar_factura(id)
-								# puts factura.to_s
-								# factura = JSON.parse(factura)
-								# puts factura.to_s
-								idfactura = factura["_id"]
-								puts "------------------------> " + factura.to_s
-								puts "------------------------> " + idfactura.to_s
-								# puts "------------------------> He generado la factura"
-								# Despachar productos
+								if validado
+									count = count + 1
+									puts "------------------------> Voy a aceptar la OC"
+									aceptar_oc(id)
+									# puts "------------------------> He aceptado oc"
+									# Generar facura para OC
+									# puts "------------------------> Voy a generar la factura"
+									factura = generar_factura(id)
+									# puts factura.to_s
+									# factura = JSON.parse(factura)
+									# puts factura.to_s
+									idfactura = factura["_id"]
+									puts "------------------------> " + factura.to_s
+									puts "------------------------> " + idfactura.to_s
+									# puts "------------------------> He generado la factura"
+									# Despachar productos
 
-								despacharInternacional(idfactura, factura)
-								puts "------------------------> "
+									despacharInternacional(idfactura, factura)
+									puts "------------------------> "
+								else
+									count = count + 1
+									puts "Ordenes procesadas:" + count.to_s
+									puts "------------------------> Voy a rechazar la OC porque el precio esta mal"
+									rechazar_oc(id)
+									# puts "------------------------> He rechazado la OC"
+								end
+							# En caso que no tengamos stock o nos pidan un producto que no procesamos rechazamos la oc
 							else
+								# puts "------------------------> No produzco el producto o no soy proveedor "
+								oc = obtener_oc(id)
+
+								oc = transform_oc(oc)
+								puts "------------------------> oc transformada: " + oc.to_s
+								oc.delete("rechazo")
+
+								localOc = Oc.new(oc)
+								localOc.save!
 								count = count + 1
 								puts "Ordenes procesadas:" + count.to_s
-								puts "------------------------> Voy a rechazar la OC porque el precio esta mal"
+								puts "------------------------> Voy a rechazar la OC porque no tengo stock o no soy proveedor: " + id.to_s
 								rechazar_oc(id)
 								# puts "------------------------> He rechazado la OC"
 							end
-						# En caso que no tengamos stock o nos pidan un producto que no procesamos rechazamos la oc
-						else
-							# puts "------------------------> No produzco el producto o no soy proveedor "
-							oc = obtener_oc(id)
-
-							oc = transform_oc(oc)
-							puts "------------------------> oc transformada: " + oc.to_s
-							oc.delete("rechazo")
-
-							localOc = Oc.new(oc)
-							localOc.save!
-							count = count + 1
-							puts "Ordenes procesadas:" + count.to_s
-							puts "------------------------> Voy a rechazar la OC porque no tengo stock o no soy proveedor: " + id.to_s
-							rechazar_oc(id)
-							# puts "------------------------> He rechazado la OC"
 						end
 					end
 				end
-				render json: {"Proceso terminado exitosamente?": true}
-			end
-		end
+			rescue => ex # En caso de excepción retornamos error
+	      logger.error ex.message
+	      puts "error 1050: " + ex.message
+	      render json: {"error": ex.message}, status: 503 and return false
+	    end
+		#end
+		render html: "Solicitud de procesamiento enviada..."
 	end
 
 	def producirMP
