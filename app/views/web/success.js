110d90abd30db2f869a1b76078e2f21911e8a245
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-06-05 17:29:27 -0400

    fix onReady execution

--- app/views/web/success.js
+++ app/views/web/success.js
@@ -1,1 +1,7 @@
-$('#despachado').html('¡Pedido despachado exitosamene!');
+var ready;
+ready = function() {
+  $('#despachado').html('¡Pedido despachado exitosamene!');
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
