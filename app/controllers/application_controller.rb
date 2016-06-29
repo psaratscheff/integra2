837f50e1131188466fece40499ab9fe97711a3a9
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-06-29 17:51:29 -0400

    fix consultar 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -645,22 +645,22 @@ class ApplicationController < ActionController::Base
   def stock_2_de_almacen(almacenId, sku)
     require 'httparty'
     begin # Intentamos realizar conexión externa y obtener OC
-    puts "--------Obteniendo Stock2 de Almacen---- :" + $urlBodega+"stock"+"?almacenId="+almacenId.to_s
+    puts "--------Obteniendo Stock2 de Almacen---- :" + $urlBodega+"skusWithStock"+"?almacenId="+almacenId.to_s
       result = HTTParty.get($urlBodega+"skusWithStock"+"?almacenId="+almacenId.to_s,
               headers: {
                 'Content-Type' => 'application/json',
                 'Authorization' => 'INTEGRACIONgrupo2:'+encode('GET'+almacenId.to_s)
               })
       puts "(Stock_2_de_Almacen)Respuesta de la contraparte: " + result.body.to_s
+      return 0 if result.body.to_s == '[]'
       json = JSON.parse(result.body)
       puts "--------Stock2 de Almacen Obtenido--------------"
-      h["incidents"].find {|h1| h1['key']=='xyz098'}['number']
       stock_count = json.find { |e| e['_id'] == sku }[total]
-      puts "STOCK DISPONIBLE: " + stock_count.to_s
+      puts "---stock disponible de este almacen: " + stock_count.to_s
       return stock_count
     rescue => ex # En caso de excepción retornamos error
       logger.error ex.message
-      puts "error 1013"
+      puts "error 18713"
       render json: { error: ex.message }, status: 503 and return
     end
   end
