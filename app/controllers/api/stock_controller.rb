243907cacb8e213b309d55da868e818be63e7e68
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-06-29 17:29:02 -0400

    comment stock sku requested

--- app/controllers/api/stock_controller.rb
+++ app/controllers/api/stock_controller.rb
@@ -2,8 +2,8 @@ class Api::StockController < ApplicationController
   include HmacHelper # Para utilizar la función de hashing
 
   def consultar
-    puts '<h1>------------------------Solicitud de consultar STOCK recibida----------------------------</h1>'
     sku = params[:sku].to_i
+    puts '<h1>------------------------Solicitud de consultar STOCK ' + sku.to_s + ' recibida----------------------------</h1>'
     stock = consultar_stock(sku) # Función definida en ApplicationController
     retorno = { stock: stock, sku: sku.to_s }.to_json
     render json: retorno
