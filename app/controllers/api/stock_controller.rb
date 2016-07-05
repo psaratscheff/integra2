78af5c0a4545040db454928a92d5ff6b1d12eb9e
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-07-05 01:19:36 -0400

    avoid unnecesary stock 0 request

--- app/controllers/api/stock_controller.rb
+++ app/controllers/api/stock_controller.rb
@@ -4,8 +4,12 @@ class Api::StockController < ApplicationController
   def consultar
     sku = params[:sku].to_i
     puts '<h1>------------------------Solicitud de consultar STOCK ' + sku.to_s + ' recibida----------------------------</h1>'
-    stock = consultar_stock(sku) # Función definida en ApplicationController
-    retorno = { stock: stock, sku: sku.to_s }.to_json
+    if sku == 0
+      retorno = { stock: 0, sku: sku.to_s }.to_json
+    else
+      stock = consultar_stock(sku) # Función definida en ApplicationController
+      retorno = { stock: stock, sku: sku.to_s }.to_json
+    end
     render json: retorno
   end
 
